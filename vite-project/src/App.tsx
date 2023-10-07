9a622d68f2b9986fb84d8068b134dac48874bdc2
Kittipol <kittipol.surapinchai@gmail.com>
2023-10-07 20:20:21 +0700

    useMemo

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,19 @@
-import { useState, useEffect, useCallback, MouseEvent, KeyboardEvent } from 'react'
+import { useState, useEffect, useCallback, MouseEvent, useMemo, KeyboardEvent } from 'react'
 
 interface User {
   id: number,
   username : string
 }
+
+type fibFunc = (n: number) => number
+
+const fib: fibFunc = (n) => {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+};
+
+const myNum: number = 37
+
 
 const App = () => {
   const [count, setCount] = useState<number>(0)
@@ -19,10 +29,13 @@ const App = () => {
   const addTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>): 
   void => setCount(prev => prev + 1),[])
 
+  const result = useMemo<number>(() => fib(myNum),[myNum])
+
   return (
     <div className="App">
       <h1>{count}</h1>
       <button onClick={addTwo}>Add</button>
+      <h2>{result}</h2>
     </div>
   )
 }
